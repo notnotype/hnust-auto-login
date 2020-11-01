04438be88b637f783840ed8dacfb9baf9698c532
notnotype <2056369669@qq.com>
2020-11-01 18:15:47 +0800

    feature: hnust.py 选择运营商登录

--- hnust.py
+++ hnust.py
@@ -21,11 +21,21 @@ def getIp():
     return socket.gethostbyname(hostname)
 
 
+def isInternetAccess():
+    successCount = 0
+    for i in range(2):
+        try:
+            successCount += 0 if "<title>上网登录页</title>" in get("http://www.baidu.com", timeout=2).text else 1
+        except:
+            pass
+    return True if successCount >= 1 else False
+
+
 def getProperties(prop):
-    f = open("./.config.txt", "a")
+    f = open("./.config", "a")
     f.close()
     kv = {}
-    f = open("./.config.txt", "r")
+    f = open("./.config", "r")
     for line in f:
         temp = line.split("=")
         kv[temp[0].strip()] = temp[1].strip()
@@ -36,13 +46,13 @@ def getProperties(prop):
 def setProperties(key, value):
     key, value = str(key), str(value)
     kv = {}
-    f = open("./.config.txt", "r")
+    f = open("./.config", "r")
     for line in f:
         temp = line.split("=")
         kv[temp[0].strip()] = temp[1].strip()
     kv[key] = value
     f.close()
-    f = open("./.config.txt", "w+")
+    f = open("./.config", "w+")
     for k, v in kv.items():
         f.write(k + " = " + v)
         f.write("\n")
@@ -60,7 +70,8 @@ def cli():
 @click.option("--username", prompt="你的学号", default=getProperties("username"))
 @click.option("--password", hide_input=True, prompt="你的校园网密码",
               default=("*" * len(getProperties("password")) if getProperties("password") else None))  # 能够回车直接输入默认值
-def login(username, password):
+@click.option("--operator", prompt="运营商选择[dx,yd,lt, xyw]", default=getProperties("operator"))
+def login(username, password, operator):
     """
     用校园网用户名（学号）和校园网密码登录校园网
     """
@@ -71,31 +82,60 @@ def login(username, password):
 
     setProperties("username", username)
     setProperties("password", password)
+    setProperties("operator", operator)
 
-    try:
-        resp = get(
-            f"http://login.hnust.cn:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C0" +
-            f"%2C{username}%40telecom&user_password={password}&wlan_user_ip={getIp()}&wlan_user_ipv6" +
-            f"=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3&v={random.randint(1000, 9999)}",
-            # 防止缓存
-            timeout=5)
-        if resp.text == r'dr1004({"result":"1","msg":"\u8ba4\u8bc1\u6210\u529f"})':
-            message = "登录成功"
-        elif resp.text == r'dr1004({"result":"0","msg":"","ret_code":2})':
-            message = "已经登录了"
-        elif "dXNlcmlkIGVycm9y" in resp.text:  # 检测userid error是否在返回值里面
-            message = "密码错误"
-        elif r"\u5bc6\u7801\u4e0d\u80fd\u4e3a\u7a7a" in resp.text:
-            message = "密码不能为空"
-        else:
-            message = "芜湖, 有bug了: " + resp.text
-    except ConnectTimeout:
-        message = "超时(你可能没有连接校园网wifi)"
-    except ConnectionError:
-        message = "找不到主机(你可能没有连接校园网wifi)"
-    except BaseException as e:
-        message = f"未知错误[{type(e)}]：\n" + str(e)
-    click.echo(message)
+    operatorMap = {"dx": "%40telecom", "yd": "%40cmcc", "lt": "%40unicom", "xyw": ""}
+
+    retry = 0
+    # todo抽象成一个方法来检测是否能上网
+    while True:  # 检测是否能够连接上网
+        try:
+            resp = get(
+                f"http://login.hnust.cn:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=%2C0" +
+                f"%2C{username}{operatorMap[operator]}&user_password={password}&wlan_user_ip={getIp()}&wlan_user_ipv6" +
+                f"=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3&v={random.randint(1000, 9999)}",
+                # 防止缓存
+                timeout=5)
+            if resp.text == r'dr1004({"result":"1","msg":"\u8ba4\u8bc1\u6210\u529f"})':
+                message = f"[登录状态]: 登录成功: 运营商是: [{operator}]"
+                click.echo(message)
+            elif resp.text == r'dr1004({"result":"0","msg":"","ret_code":2})':
+                message = "[登录状态]: 已经登录了"
+                click.echo(message)
+                click.echo("退出登录中......")
+                _logOut()
+                continue  # 都已经退出登录了,就不要在检测是否可以连接至internet了
+            elif "dXNlcmlkIGVycm9y" in resp.text:  # 检测userid error是否在返回值里面
+                message = "[登录状态]: 密码错误(检查是否有绑定运营商账号)"
+                click.echo(message)
+                break
+            elif r"\u5bc6\u7801\u4e0d\u80fd\u4e3a\u7a7a" in resp.text:
+                message = "[登录状态]: 密码不能为空"
+                click.echo(message)
+                break
+            else:
+                message = "[登录状态]: 芜湖, 有bug了: " + resp.text
+                click.echo(message)
+                break
+
+            click.echo("检测是否可以连接到互联网......")
+            if isInternetAccess():
+                click.echo("可以连接互联网  登陆成功")
+                return
+            else:
+                if retry >= 3:
+                    click.echo("该账号不能连接至Internet(你可能使用校园网登录,因此不能连接至互联网)")
+                    return
+                click.echo("不能连接互联网, 重试")
+                retry += 1
+        except ConnectTimeout:
+            message = "超时(你可能没有连接校园网wifi)"
+        except ConnectionError:
+            message = "找不到主机(你可能没有连接校园网wifi)"
+        except BaseException as e:
+            message = f"未知错误[{type(e)}]：\n" + str(e)
+
+
 
 
 # todo 返回网页包含了用户网络信息，使用re库来获取script标签里面的变量
@@ -109,11 +149,7 @@ def getInfo():
     print(resp.text)
 
 
-# noinspection PyBroadException
-@click.command()
-@click.confirmation_option(prompt=f"你确定要注销登陆【你当前IP为：{getIp()}】", default=True)
-def logOut():
-    """退出登录"""
+def _logOut():
     try:
         resp = get(f"http://login.hnust.cn:801/eportal/?c=Portal&a=logout&callback=dr1003&login_method=1&user_account" +
                    f"=drcom&user_password=123&ac_logout=0&register_mode=1&wlan_user_ip={getIp()}&wlan_user_ipv6" +
@@ -137,6 +173,15 @@ def logOut():
 
 # noinspection PyBroadException
 @click.command()
+@click.confirmation_option(prompt=f"你确定要注销登陆【你当前IP为：{getIp()}】", default=True)
+def logOut():
+    """退出登录"""
+    _logOut()
+
+
+
+# noinspection PyBroadException
+@click.command()
 def addStartup():
     """把这个程序添加到开机启动里面 这样每次开机就能够自动检测登录了"""
 
@@ -144,7 +189,7 @@ def addStartup():
         os.popen("explorer.exe \"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp")
         click.echo("记得使用管理员的身份运行")
         cmd1 = f"copy hnust.py \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.py\""
-        cmd2 = f"copy .config.txt \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\.config.txt\""
+        cmd2 = f"copy .config \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\.config\""
         cmd3 = f"copy hnust.exe \"C:\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\hnust.exe\""
 
         click.echo("cmd1: " + cmd1)
@@ -174,7 +219,9 @@ cli.add_command(addStartup)
 
 if __name__ == '__main__':
     if "StartUp" in sys.argv[0] and len(sys.argv) <= 1:  # 如果在启动目录下则自动检测登录
-        sys.argv = [sys.argv[0], "login", "--username",
-                    getProperties("username") if getProperties("username") else None,
-                    "--password", getProperties("password") if getProperties("password") else None]
+        sys.argv = [sys.argv[0], "login",
+                    "--username", getProperties("username") if getProperties("username") else None,
+                    "--password", getProperties("password") if getProperties("password") else None,
+                    "--operator", getProperties("operator") if getProperties("operator") else None,
+                    ]
     cli()
